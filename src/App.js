19c777d75fad56300a546d4bb07cb4d3d9d519cf
Sein Izumita <sein.izumita@gmail.com>
2023-02-09 10:58:36 -0500

    adding movie api to display movies on search

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
+import { useEffect } from "react";
 import React from "react";
 
+//83f5033f
+
+const API_URL ='http://www.omdbapi.com?apikey=83f5033f'
+
+    
+
 const App = () =>{
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('The Room');
+
+    }, [])
+
+
+
     return(
         <h1>App</h1>
     );
